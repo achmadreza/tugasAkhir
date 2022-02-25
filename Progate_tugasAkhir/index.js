a518a6f652710daa60056119ac3050a3090c5d18
achmadreza <areszha@gmail.com>
2022-02-25 10:01:00 +0700

    first commit

---
+++ Progate_tugasAkhir/index.js
@@ -0,0 +1,46 @@
+const searchSong= document.querySelector(".search");
+const songsContainer = document.querySelector(".song-of-song");
+
+const search = async (e) => {
+	e.preventDefault();
+	let keyword = searchSong["artist"].value;
+	const response = await fetch(`https://genius.p.rapidapi.com/search?q=${keyword}`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "genius.p.rapidapi.com",
+			"x-rapidapi-key": process.env.MY_API-TOKEN
+		}
+	});
+
+	if(response.status === 200) {
+		const resultResponse = await response.json();
+		if(resultResponse.meta.status === 200) {
+			songsContainer.innerHTML = "";
+			resultResponse.response.hits.forEach(listSongs);
+		}
+	} else {
+		console.error("Terjadi kesalahan");
+			
+    }
+	
+}
+const listSongs = (e) => {
+	console.log(e);
+	songsContainer.innerHTML += `<div class="row">
+    <div class="col-md-6">
+    <div class="song-card">
+        <div class="card" style="width: 16rem;">
+  <img src="${e.result.song_art_image_thumbnail_url}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">${e.result.artist_names}</p>
+    <p class="card-text"> ${e.result.title}</p>
+    <a href="${e.result.url}" class="btn btn-primary">Go Lyrics </a>
+  </div>
+  </div>
+  </div>
+  </div>
+</div>`
+}
+
+searchSong.addEventListener("submit", search);
+
